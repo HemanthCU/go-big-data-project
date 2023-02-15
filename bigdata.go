b68fb0343ff0f2f33a2f77e6e4f8ccaba40591ab
HemanthCU <hech9374@colorado.edu>
2023-02-14 17:45:04 -0700

    HTTP call

--- bigdata.go
+++ bigdata.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"sync"
 
 	getapi "github.com/HemanthCU/go-big-data-project/getapi"
 )
 
+func getData(url string) string {
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	res := string(body)
+	return res
+}
+
 func main() {
 	var val float64
 	var wg sync.WaitGroup
+	var url string
 	fmt.Println("Enter a value greater than 4 to see more")
 	fmt.Scanln(&val)
 	if val > 4 {
@@ -22,5 +39,10 @@ func main() {
 		fmt.Println("main")
 		wg.Wait()
 	}
+
+	url = "http://api.worldbank.org/v2/countries/BGD/indicators/NY.GDP.MKTP.KD.ZG?per_page=11&date=2000:2010"
+	data := getData(url)
+	fmt.Println(data)
+
 	fmt.Println("all code complete")
 }
